98b9026eadd16d4c46e945120320646d6a856ec8
anushree200 <aanuushree@gmail.com>
2025-06-16 03:09:59 +0530

    some changes

--- event-registration-app/src/components/navbar.jsx
+++ event-registration-app/src/components/navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     <nav className="bg-blue-600 text-white p-4 flex justify-between">
       <h1 className="text-xl font-bold">Event Manager</h1>
       <div className="space-x-4">
-        <a href="#" className="hover:underline">Home</a>
+        <a href="#" className="hover:underline pr-4">Home</a>
         <a href="#" className="hover:underline">My Events</a>
         <a href="#" className="hover:underline">Register</a>
       </div>
